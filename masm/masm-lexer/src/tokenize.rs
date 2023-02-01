f0c8af11767f2b1d297abf12e2e0336e78c71a3e
Marek Vospěl <marek@vospel.cz>
2023-02-01 22:11:56 +0100

    feat: add double quote strings

--- masm/masm-lexer/src/tokenize.rs
+++ masm/masm-lexer/src/tokenize.rs
@@ -31,18 +31,26 @@ fn save_whitespace(collected: &mut String, tokens: &mut Vec<Token>, index: usize
     }
 }
 
-fn save_string(collected: &mut String, tokens: &mut Vec<Token>, index: usize, double: bool) {
+fn save_string(
+    collected: &mut String,
+    tokens: &mut Vec<Token>,
+    index: usize,
+    double: bool,
+    escaped: &mut usize,
+) {
     if collected.len() > 0 {
+        let str = collected.clone();
         tokens.push(Token {
             token: if double {
                 Tokens::DoubleQuoteString(collected.clone())
             } else {
                 Tokens::SingleQuoteString(collected.clone())
             },
-            start: index - collected.len(),
+            start: index - collected.len() - *escaped,
             end: index - 1,
-            src: collected.clone(),
+            src: str.clone(),
         });
+        *escaped = 0;
         *collected = String::new();
     }
 }
@@ -53,9 +61,10 @@ pub fn tokenize<S: Into<String>>(code: S) -> Vec<Token> {
 
     let mut escape_active = false;
     let mut quote_active = false;
-    // let mut double_quote_active = false;
+    let mut double_quote_active = false;
     let mut whitespace_active = false;
     let mut comment_active = false;
+    let mut escaped = 0;
 
     let mut collected = String::new();
 
@@ -69,14 +78,16 @@ pub fn tokenize<S: Into<String>>(code: S) -> Vec<Token> {
             }
         }
 
-        if escape_active {
+        if escape_active && (quote_active || double_quote_active) {
             collected.push(char);
+            escaped += 1;
+            escape_active = false;
             continue;
         }
 
         match char {
             ';' => {
-                if !quote_active {
+                if !quote_active && !double_quote_active {
                     comment_active = true;
                     if whitespace_active {
                         whitespace_active = false;
@@ -95,43 +106,80 @@ pub fn tokenize<S: Into<String>>(code: S) -> Vec<Token> {
                 }
             }
             '\\' => {
-                if quote_active {
+                if quote_active || double_quote_active {
                     escape_active = true;
+                    continue;
                 }
             }
             '\'' => {
-                if !quote_active {
-                    if comment_active {
-                        save_comment(&mut collected, &mut tokens, i);
-                        whitespace_active = false
-                    } else if whitespace_active {
-                        save_whitespace(&mut collected, &mut tokens, i);
-                        whitespace_active = false
+                if !double_quote_active {
+                    if !quote_active {
+                        if comment_active {
+                            save_comment(&mut collected, &mut tokens, i);
+                            whitespace_active = false
+                        } else if whitespace_active {
+                            save_whitespace(&mut collected, &mut tokens, i);
+                            whitespace_active = false
+                        } else {
+                            save_collected(&mut collected, &mut tokens, i);
+                        }
+                        tokens.push(Token {
+                            token: Tokens::SingleQuote,
+                            start: i,
+                            end: i,
+                            src: "'".into(),
+                        });
+                        quote_active = true;
+                        continue;
                     } else {
-                        save_collected(&mut collected, &mut tokens, i);
+                        save_string(&mut collected, &mut tokens, i, false, &mut escaped);
+                        tokens.push(Token {
+                            token: Tokens::SingleQuote,
+                            start: i,
+                            end: i,
+                            src: "'".into(),
+                        });
+                        quote_active = false;
+                        continue;
                     }
-                    tokens.push(Token {
-                        token: Tokens::SingleQuote,
-                        start: i,
-                        end: i,
-                        src: "'".into(),
-                    });
-                    quote_active = true;
-                    continue;
-                } else {
-                    save_string(&mut collected, &mut tokens, i, false);
-                    tokens.push(Token {
-                        token: Tokens::SingleQuote,
-                        start: i,
-                        end: i,
-                        src: "'".into(),
-                    });
-                    quote_active = false;
-                    continue;
                 }
             }
+            '\"' => {
+                if !quote_active {
+                    if !double_quote_active {
+                        if comment_active {
+                            save_comment(&mut collected, &mut tokens, i);
+                            whitespace_active = false
+                        } else if whitespace_active {
+                            save_whitespace(&mut collected, &mut tokens, i);
+                            whitespace_active = false
+                        } else {
+                            save_collected(&mut collected, &mut tokens, i);
+                        }
+                        tokens.push(Token {
+                            token: Tokens::DoubleQuote,
+                            start: i,
+                            end: i,
+                            src: "\"".into(),
+                        });
+                        double_quote_active = true;
+                        continue;
+                    } else {
+                        save_string(&mut collected, &mut tokens, i, true, &mut escaped);
+                        tokens.push(Token {
+                            token: Tokens::DoubleQuote,
+                            start: i,
+                            end: i,
+                            src: "\"".into(),
+                        });
+                        double_quote_active = false;
+                        continue;
+                    }
+                }
+            }
+
             ' ' | '\r' => {
-                if !whitespace_active && !comment_active && !quote_active {
+                if !whitespace_active && !comment_active && !quote_active && !double_quote_active {
                     save_collected(&mut collected, &mut tokens, i);
                     whitespace_active = true;
                 }
@@ -170,7 +218,9 @@ pub fn tokenize<S: Into<String>>(code: S) -> Vec<Token> {
     } else if whitespace_active {
         save_whitespace(&mut collected, &mut tokens, code.len())
     } else if quote_active {
-        save_string(&mut collected, &mut tokens, code.len(), false)
+        save_string(&mut collected, &mut tokens, code.len(), false, &mut escaped)
+    } else if double_quote_active {
+        save_string(&mut collected, &mut tokens, code.len(), true, &mut escaped)
     }
 
     tokens
--- masm/masm-lexer/tests/strings.rs
+++ masm/masm-lexer/tests/strings.rs
@@ -48,3 +48,80 @@ fn should_have_string() {
 
     assert_eq!(tokenize("  db 'Hello world'"), assertion);
 }
+
+#[test]
+fn should_have_double_string() {
+    let mut assertion = Vec::new();
+
+    assertion.push(Token {
+        token: Tokens::Whitespace,
+        src: "  ".into(),
+        start: 0,
+        end: 1,
+    });
+
+    assertion.push(Token {
+        token: Tokens::Literal("db".into()),
+        src: "db".into(),
+        start: 2,
+        end: 3,
+    });
+
+    assertion.push(Token {
+        token: Tokens::Whitespace,
+        src: " ".into(),
+        start: 4,
+        end: 4,
+    });
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuote,
+        src: "\"".into(),
+        start: 5,
+        end: 5,
+    });
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuoteString("Hello world".into()),
+        src: "Hello world".into(),
+        start: 6,
+        end: 16,
+    });
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuote,
+        src: "\"".into(),
+        start: 17,
+        end: 17,
+    });
+
+    assert_eq!(tokenize("  db \"Hello world\""), assertion);
+}
+
+#[test]
+fn should_allow_special_chars_in_string() {
+    let mut assertion = Vec::new();
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuote,
+        src: "\"".into(),
+        start: 0,
+        end: 0,
+    });
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuoteString(" $'\"".into()),
+        src: " $'\"".into(),
+        start: 1,
+        end: 5,
+    });
+
+    assertion.push(Token {
+        token: Tokens::DoubleQuote,
+        src: "\"".into(),
+        start: 6,
+        end: 6,
+    });
+
+    assert_eq!(tokenize("\" $'\\\"\""), assertion)
+}
